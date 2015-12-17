c542924d47da0321b46339a959044bd4860aa698
abcxyz09 <jonyroy16@gmail.com>
2015-12-17 19:03:14 +0600

    Version-1.0

---
+++ MyFirstProject/FindDay.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int Day,Year,Month;
+    cout<<"Enter The Date Year Month Day=";
+    cin>>Year>>Month>>Day;
+
+    return 0;
+}
