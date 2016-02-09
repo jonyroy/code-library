08c2b59cba8730e343b3bd2c304043809de36932
abcxyz09 <jonyroy16@gmail.com>
2016-02-09 17:52:47 +0600

    Version-1.0

--- ShahinVaiCode/Datastructure/MERGE-SORT.cpp
+++ ShahinVaiCode/Datastructure/MERGE-SORT.cpp
@@ -23,25 +23,26 @@ void MERGE(int A[],int p,int q,int r)
     n1=q-p+1;
     n2=r-q;
     int L[n1+1],R[n2+1];
-    for(i=0;i<n1;i++)
-    L[i]=A[p+i];
-    for(i=0;i<n2;i++)
-    R[i]=A[q+i+1];
-    L[n1]=max_;R[n2]=max_;
+    for(i=0; i<n1; i++)
+        L[i]=A[p+i];
+    for(i=0; i<n2; i++)
+        R[i]=A[q+i+1];
+    L[n1]=max_;
+    R[n2]=max_;
     i=0,j=0;
-    for(k=p;k<=r;k++)
+    for(k=p; k<=r; k++)
     {
         if(L[i]<=R[j])
-        A[k]=L[i],i++;
+            A[k]=L[i],i++;
         else
-        A[k]=R[j],j++;
+            A[k]=R[j],j++;
     }
 }
 void MERGE_SORT(int A[],int p,int r)
 {
     if(p<r)
     {
-       int q=(p+r)/2;
+        int q=(p+r)/2;
         MERGE_SORT(A,p,q);
         MERGE_SORT(A,q+1,r);
         MERGE(A,p,q,r);
@@ -49,13 +50,13 @@ void MERGE_SORT(int A[],int p,int r)
 }
 int main()
 {
-int i,j,n,key;
-int A[500];
-n=5;
-for(j=0;j<n;j++)
- scanf("%d",&A[j]);
-MERGE_SORT(A,0,n-1);
- for(j=0;j<n;j++)
-  printf("%d ",A[j]);
+    int i,j,n,key;
+    int A[500];
+    n=5;
+    for(j=0; j<n; j++)
+        scanf("%d",&A[j]);
+    MERGE_SORT(A,0,n-1);
+    for(j=0; j<n; j++)
+        printf("%d ",A[j]);
     return 0;
 }
