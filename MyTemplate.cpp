c46182b2c5528de134c3f4cf0c832a5ddaee79ed
abcxyz09 <jonyroy16@gmail.com>
2016-02-11 22:54:42 +0600

    Version-1.0

---
+++ MyTemplate.cpp
@@ -0,0 +1,30 @@
+/*
+* Author: Jony Roy
+* Date: 07-02-2016
+* Contact: jonyroyice@gmail.com
+*/
+#include<bits/stdc++.h>
+#define PI 3.1415926535897932384
+#define p_b(x) push_back(x)
+#define m_p(x) make_pair(x)
+#define ft first
+#define sd second
+using namespace std;
+typedef long long int llint;
+typedef unsigned long long int ullint;
+typedef long int lint;
+typedef unsigned int uint;
+typedef unsigned long int ulint;
+typedef pair<int,int> pint;
+typedef pair<lint,lint> plint;
+typedef pair<llint,llint> pllint;
+
+int main()
+{
+    int a;
+    cin>>a;
+    if(a<0)
+        a=-a;
+    cout<<a<<endl;
+    return 0;
+}
