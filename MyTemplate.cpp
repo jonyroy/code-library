ebf9717d449fe819c0552ef3d5ca68ffcafb28fc
abcxyz09 <jonyroy16@gmail.com>
2016-02-20 10:42:36 +0600

    v

--- MyTemplate.cpp
+++ MyTemplate.cpp
@@ -1,30 +1,28 @@
 /*
 * Author: Jony Roy
-* Date: 07-02-2016
+* Date: 17-02-2016
 * Contact: jonyroyice@gmail.com
 */
-#include<bits/stdc++.h>
-#define PI 3.1415926535897932384
-#define p_b(x) push_back(x)
-#define m_p(x) make_pair(x)
-#define ft first
-#define sd second
+#include <bits/stdc++.h>
+#define PI              3.1415926535897932384
+#define EulerConstant   0.5772156649015328606
+#define CountBit(a)     __builtin_popcount(a)
+#define p_b(x)          push_back(x)
+#define m_p(x)          make_pair(x)
+#define ft              first
+#define sd              second
 using namespace std;
-typedef long long int llint;
-typedef unsigned long long int ullint;
-typedef long int lint;
-typedef unsigned int uint;
-typedef unsigned long int ulint;
-typedef pair<int,int> pint;
-typedef pair<lint,lint> plint;
-typedef pair<llint,llint> pllint;
+typedef long long int            llint;
+typedef unsigned long long int   ullint;
+typedef long int                 lint;
+typedef unsigned int             uint;
+typedef unsigned long int        ulint;
+typedef pair<int,int>            pint;
+typedef pair<lint,lint>          plint;
+typedef pair<llint,llint>        pllint;
 
-int main()
+int main(int argc, char const *argv[])
 {
-    int a;
-    cin>>a;
-    if(a<0)
-        a=-a;
-    cout<<a<<endl;
+    
     return 0;
 }
