56e8f6767050662512aa313110d66947d9598768
abcxyz09 <jonyroy16@gmail.com>
2016-06-28 12:46:29 +0600

    latest

--- MyTemplate.cpp
+++ MyTemplate.cpp
@@ -1,14 +1,14 @@
 /*
 * Author: Jony Roy
-* Date: 21-02-2016
+* Date: 22-06-2016
 * Contact: jonyroyice@gmail.com
 */
 #include <bits/stdc++.h>
 #define PI              3.1415926535897932384
 #define EulerConstant   0.5772156649015328606
 #define CountBit(a)     __builtin_popcount(a)
-#define p_b(x)          push_back(x)
-#define m_p(x)          make_pair(x)
+#define pb(x)          push_back(x)
+#define mp(x)          make_pair(x)
 #define ft              first
 #define sd              second
 using namespace std;
@@ -22,11 +22,84 @@ typedef pair<int,int>            pint;
 typedef pair<lint,lint>          plint;
 typedef pair<llint,llint>        pllint;
 
+/*The Debugger Template used Here is written by Bidhan Roy*/
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p )
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v )
+{
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ )
+    {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v )
+{
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ )
+    {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v )
+{
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ )
+    {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+#define deb(x) cerr << #x << " = " << x << endl;
+/*..........................................................................*/
+
+template<class T> T gcd(T a,T b){if(b == 0)return a;return gcd(b,a%b);}
+template<class T> T lcm(T a, T b ){return (a*b)/gcd(a,b);}
+
+//int fx[]={0,-1,0,1};
+//int fy[]={-1,0,1,0};
+
+llint bigMod(llint a,llint b,llint MOD)
+{
+    if(b==0)
+        return 1;
+    llint temp=bigMod(a,b>>1,MOD);
+    temp=(temp*temp)%MOD;
+    if(b&1)
+        temp= (a*temp)%MOD;
+    return temp;
+}
+
 int main(int argc, char *argv[])
 {
-        for (int i = 1; i <= argc; ++i)
-        {
-            cout<<argv[i]<<endl;
-        }
-        return 0;
+    ios_base::sync_with_stdio(0);
+    vector<int> a;
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        int b;
+        cin>>b;
+        a.push_back(b);
+    }
+    deb(a);
+    return 0;
 }
